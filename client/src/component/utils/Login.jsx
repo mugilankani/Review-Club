ba4753a6f430dc739c6998cfd8112be670e542c1
mugilankani <mugilankani951@gmail.com>
2024-07-22 10:37:37 +0530

    fixed the bug in google auth

--- client/src/component/utils/Login.jsx
+++ client/src/component/utils/Login.jsx
@@ -2,41 +2,41 @@ import React from "react";
 import axios from "axios";
 
 export default function LoginPage() {
-	const handleGoogleLogin = () => {
-		axios.get(`/auth/google`).then((response) => {
-			window.location.href = response.data.url;
-		});
-	};
+  const handleGoogleLogin = () => {
+    axios
+      .get(`${import.meta.env.VITE_API_URL}/auth/google`)
+      .then((response) => {
+        window.location.href = response.data.url;
+      });
+  };
 
-	return (
-		<div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
-			<div className="sm:mx-auto sm:w-full sm:max-w-md">
-				<h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-					Welcome to Review Club
-				</h2>
-				<p className="mt-2 text-center text-sm text-gray-600">
-					Sign in to access your account
-				</p>
-			</div>
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+          Welcome to Review Club
+        </h2>
+        <p className="mt-2 text-center text-sm text-gray-600">
+          Sign in to access your account
+        </p>
+      </div>
 
-			<div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-				<div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-					<div className="space-y-6">
-						<button
-							onClick={handleGoogleLogin}
-							className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-						>
-							<img
-								className="mr-2 h-5 w-5 rounded-full"
-								src={`https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjzC2JyZDZ_RaWf0qp11K0lcvB6b6kYNMoqtZAQ9hiPZ4cTIOB`}
-							/>
-							Sign in with Google
-						</button>
-					</div>
-
-					
-				</div>
-			</div>
-		</div>
-	);
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+          <div className="space-y-6">
+            <button
+              onClick={handleGoogleLogin}
+              className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              <img
+                className="mr-2 h-5 w-5 rounded-full"
+                src={`https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjzC2JyZDZ_RaWf0qp11K0lcvB6b6kYNMoqtZAQ9hiPZ4cTIOB`}
+              />
+              Sign in with Google
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
